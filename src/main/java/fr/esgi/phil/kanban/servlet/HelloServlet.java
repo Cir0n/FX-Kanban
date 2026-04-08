3d975ead8d27436b007f3d9631de5f84df0af881
Cir0n <philippe.baba3000@gmail.com>
2026-04-08 10:33:41 +0200

    initial commit

---
+++ src/main/java/fr/esgi/phil/kanban/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package fr.esgi.phil.kanban.servlet;
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+@WebServlet(name = "helloServlet", value = {"/index", "/"})
+public class HelloServlet extends HttpServlet {
+    private String message;
+    private TemplateEngine templateEngine = null;
+    public void init() {
+        IO.println("Initialisation de la servlet HelloServlet");
+        message = "Hello Kanban";
+        // On récupère le moteur de template dans le contexte des servlets
+        templateEngine = (TemplateEngine) getServletContext().getAttribute("templateEngine");
+    }
+    // la méthode doGet ci-dessous sera invoquée par Tomcat
+    // Tomcat invoque cette méthode car elle prend en charge toutes
+    // les requêtes HTTP utilisant la méthode GET vers l'url index et vers l’url /
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // On crée un context Thymeleaf qui va accueillir les objets Java
+        // qui seront envoyés à la vue Thymeleaf
+        Context context = new Context();
+        // On invoque la méthode process qui formule la réponse qui sera renvoyée au navigateur
+        templateEngine.process("hello", context, response.getWriter());
+    }
+    public void destroy() {
+    }
+}
